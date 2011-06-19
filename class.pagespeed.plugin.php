e8b53fa495ab9604b56e81485f69c68024960672
S <unlights@gmail.com>
2011-06-19 15:31:08 +0400

    Merge commit '3e038fdf82cc43fbe2b4a1b69beb82899aa2b73a'

--- class.pagespeed.plugin.php
+++ class.pagespeed.plugin.php
@@ -2,39 +2,97 @@
 
 $PluginInfo['PageSpeed'] = array(
 	'Name' => 'Page Speed',
-	'Description' => 'Minimizes payload size (compressing css/js files), minimizes round-trip times (loads JQuery library from CDN, combines external JavaScript/CSS files). Inspired by Google Page Speed rules. See readme.txt for details.',
-	'Version' => '1.4t',
+	'Description' => 'Minimizes payload size (compressing css/js files), minimizes round-trip times (loads JQuery library from CDN, combines external JavaScript/CSS files). Inspired by Google Page Speed rules. See readme for details.',
+	'Version' => '1.4.19',
 	'Date' => 'Summer 2011',
-	'Author' => 'S',
+	'Author' => 'Nobody',
 	'AuthorUrl' => 'https://github.com/search?type=Repositories&language=php&q=PageSpeed',
-	'RequiredApplications' => False,
+	'RequiredApplications' => array('Dashboard' => '>=2.0.18'),
 	'RequiredTheme' => False, 
 	'RequiredPlugins' => array('UsefulFunctions' => '>=2.3.60'),
 	'RegisterPermissions' => False,
 	'SettingsPermission' => False
 );
 
+// TODO: REMOVE WHEN 2.0.18
+if (!function_exists('AbsoluteSource')) {
+/**
+* Takes a source path (ie. an image src from an html page), and an
+* associated URL (ie. the page that the image appears on), and returns the
+* absolute source (including url & protocol) path.
+* @param string $SrcPath The source path to make absolute (if not absolute already).
+* @param string $Url The full url to the page containing the src reference.
+* @return string Absolute source path.
+*/
+	function AbsoluteSource($SrcPath, $Url) {
+		// If there is a scheme in the srcpath already, just return it.
+		if (!is_null(parse_url($SrcPath, PHP_URL_SCHEME))) return $SrcPath;
+		
+		// Does SrcPath assume root?
+		if (in_array(substr($SrcPath, 0, 1), array('/', '\\')))
+			return parse_url($Url, PHP_URL_SCHEME)
+				.'://'
+				.parse_url($Url, PHP_URL_HOST)
+				.$SrcPath;
+	
+		// Work with the path in the url & the provided src path to backtrace if necessary
+		$UrlPathParts = explode('/', str_replace('\\', '/', parse_url($Url, PHP_URL_PATH)));
+		$SrcParts = explode('/', str_replace('\\', '/', $SrcPath));
+		$Result = array();
+		foreach ($SrcParts as $Part) {
+			if (!$Part || $Part == '.') continue;
+			if ($Part == '..') array_pop($UrlPathParts);
+			else $Result[] = $Part;
+		}
+		// Put it all together & return
+		return parse_url($Url, PHP_URL_SCHEME)
+			.'://'
+			.parse_url($Url, PHP_URL_HOST)
+			.'/'.implode('/', array_filter(array_merge($UrlPathParts, $Result)));
+	}
+}
+
 class PageSpeedPlugin implements Gdn_IPlugin {
 	
-	protected static function RemoveKeyFromArray($Array, $Keys) {
-		// RemoveKeyFromArray in functions.general.php doesnt work as expected
-		if (!is_array($Keys)) $Keys = array($Keys);
-		if (is_array($Array)) foreach ($Keys as $Key) unset($Array[$Key]);
-		return $Array;
+	private $bRenderInitialized = False;
+	private $Configuration = array();
+	
+	public function __construct() {
+		$this->Configuration = C('Plugins.PageSpeed');
 	}
 	
-	protected static function HashSumFiles($Files) {
-		$Files = array_unique($Files);
-		$HashSum = array_sum(array_map('crc32', $Files));
-		$NewHash = sprintf('%u', crc32($HashSum));
-		return $NewHash;
+	public function Base_Render_Before($Sender) {
+		$EnablePostProcessing = GetValue('SetImageDimensions', $this->Configuration)
+			&& $Sender->DeliveryMethod() == DELIVERY_METHOD_XHTML
+			&& $Sender->DeliveryType() == DELIVERY_TYPE_ALL 
+			&& $Sender->SyndicationMethod == SYNDICATION_NONE;
+		if ($EnablePostProcessing) {
+			ob_start();
+			$this->bRenderInitialized = True;
+		}
+	}
+	
+	public function Base_Render_After($Sender) {
+		if ($this->bRenderInitialized) {
+			$String = ob_get_contents();
+			ob_end_clean();
+			self::StaticDomDocumentReplace($String);
+			//$Gdn_Timer = new Gdn_Timer();
+/*			$Gdn_Timer->Start();
+			self::StaticDomDocumentReplace($String);
+			$Gdn_Timer->Finish('self::StaticDomDocumentReplace($String);');*/
+			/*$Gdn_Timer->Start();
+			self::StaticPhpQueryReplace($String);
+			$Gdn_Timer->Finish('self::StaticPhpQueryReplace($String);');*/
+			echo $String;
+		}
 	}
 	
 	public function HeadModule_BeforeToString_Handler($Head) {
-		
-		$Configuration = C('Plugins.PageSpeed');
 
-		if (defined('DEBUG') && !GetValue('IgnoreDebug', $Configuration)) return;
+		$Configuration =& $this->Configuration;
+		$Debug = C('Debug');
+		if ($Debug && !GetValue('IgnoreDebug', $Configuration)) return;
 		
 		$CombinedJavascript = array('library' => array());
 		$CombinedCss = array();
@@ -47,26 +105,8 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 			// JavaScript (script tag)
 			if (GetValue(HeadModule::TAG_KEY, $Tag) == 'script') {
 				if (!isset($JsTag)) $JsTag = $Tag;
-				
-				$Src =& $Tag['src']; 
-				if (self::IsUrl($Src)) continue;
-				$Path = parse_url($Src, PHP_URL_PATH);
-				$Basename = pathinfo($Path, PATHINFO_BASENAME);
-				if ($Basename == 'jquery.js') {
-					$Version = GetValueR('CDN.jquery', $Configuration, '1.4.1');
-					//if (Gdn_Statistics::IsLocalhost()) continue; // TODO: CONF TO DISABLE
-					$Src = 'http://ajax.googleapis.com/ajax/libs/jquery/'.$Version.'/jquery.min.js';
-					continue;
-				} elseif ($Basename == 'jqueryui.js' || $Basename == 'jquery.ui.packed.js') {
-					$Version = GetValueR('CDN.jqueryui', $Configuration, '1.7.1');
-					$Src = 'http://ajax.googleapis.com/ajax/libs/jqueryui/'.$Version.'/jquery-ui.min.js';
-					continue;
-				}
-				
-				$FilePath = PATH_ROOT.$Path;
-				if (!file_exists($FilePath)) trigger_error("No such file ($FilePath)");
-				$Hash = Crc32Value(md5_file($FilePath), filemtime($FilePath));
-				$CachedFilePath = "cache/ps/{$Hash}.$Basename";
+				$CachedFilePath = $this->GetCachedFilePath($Tag, 'src', $FilePath);
+				if ($CachedFilePath === False) continue;
 				
 				if (!file_exists($CachedFilePath)) {
 					if (!isset($Snoopy)) $Snoopy = Gdn::Factory('Snoopy');
@@ -76,46 +116,25 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 						'js_code' => file_get_contents($FilePath)
 					));
 					if ($Snoopy->error) {
-						trigger_error($Snoopy->error);
-						$Snoopy->error = Null;
+						if ($Debug) trigger_error($Snoopy->error);
+						$Snoopy->error = '';
 						continue;
 					}
 					file_put_contents($CachedFilePath, $Snoopy->results);
 				}
 				
-				$GroupName = self::GetGroupName($Path);
-				if ($GroupName == 'js' || 
-					in_array($Basename, array('global.js', 'queue.js', 'slice.js'))) $GroupName = 'library';
-				elseif ($GroupName == 'themes') $GroupName = 'plugins';
-				elseif (!in_array($GroupName, array('plugins', 'applications', 'library')))
-					$GroupName = 'applications'; // Unknown group, move it to application group
+				$GroupName = self::GetGroupName($FilePath);
+				if ($GroupName == 'js' || $GroupName == 'themes') $GroupName = 'library';
+				elseif (!in_array($GroupName, array('plugins', 'applications', 'library'))) {
+					// Unknown group, move it to application group.
+					$GroupName = 'applications';
+				}
 				$CombinedJavascript[$GroupName][$Index] = $CachedFilePath;
 				
 			} elseif (GetValue(HeadModule::TAG_KEY, $Tag) == 'link' && GetValue('rel', $Tag) == 'stylesheet') {
-				
-				//$Version = GetValueR('CDN.jqueryui', $Configuration, '1.5.2');
-				// TODO:
-				//http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.13/themes/smoothness/jquery-ui.css
-				
-				// Css (link/stylesheet tag)
-				// We must save to same directory becase url(relativepath)
-				// You can cleanup cached files by running script
 
-				$Href =& $Tag['href'];
-				if (self::IsUrl($Href)) continue;
-				
-				$Path = parse_url($Href, PHP_URL_PATH);
-				$Basename = pathinfo($Path, PATHINFO_BASENAME);
-				// TODO: custom-
-/*				if (substr($Basename, 0, 6) == 'custom') {
-					d($Tags, $Basename);
-				}*/
-				
-				$FilePath = PATH_ROOT.$Path;
-				if (!file_exists($FilePath)) trigger_error("No such file ($FilePath)");
-				$Hash = Crc32Value(md5_file($FilePath), filemtime($FilePath));
-
-				$CachedFilePath = "cache/ps/{$Hash}.$Basename";
+				$CachedFilePath = $this->GetCachedFilePath($Tag, 'href', $FilePath);
+				if ($CachedFilePath === False) continue;
 
 				if (!file_exists($CachedFilePath)) {
 					$Css = file_get_contents($FilePath);
@@ -129,7 +148,9 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 							$UrlImage = trim($UrlImage, '"\'');
 							if ($UrlImage[0] == '/' || self::IsUrl($UrlImage) || substr($UrlImage, 0, 5) == 'data:') continue;
 							$File = dirname($FilePath).'/'.$UrlImage;
-							if (!file_exists($File)) trigger_error("Error while fix background image url path. No such file ($File).");
+							if (!file_exists($File)) {
+								if ($Debug) trigger_error("Error while fix background image url path. No such file ($File).");
+							}
 							$Asset = Asset(substr($File, strlen(PATH_ROOT)+1));
 							$CssText = str_replace($Match[0][$N], "url($Asset)", $CssText);
 						}
@@ -141,19 +162,15 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 					file_put_contents($CachedFilePath, $CssText);
 				}
 				
-				$GroupName = self::GetGroupName($Path);
+				$GroupName = self::GetGroupName($FilePath);
 				// combine in two group applications and plugins
-				// TODO: ATTENTION! "screen" changed to "all" in unstable
-				$Media = GetValue('media', $Tag);
-				if ($Media == 'screen' || $Media == 'all') {
-					if (!isset($CssTag)) $CssTag = $Tag;
-					if (!in_array($GroupName, array('plugins', 'applications'))) 
-						$GroupName = 'applications';
-					$CombinedCss[$GroupName][$Index] = $CachedFilePath;
-				} else 
-					$Href = Asset($CachedFilePath);
+				if (!isset($CssTag)) $CssTag = $Tag;
+				if (!in_array($GroupName, array('plugins', 'applications'))) $GroupName = 'applications';
+				$CombinedCss[$GroupName][$Index] = $CachedFilePath;
 			}
 		}
+		
+		//d(@$CombinedCss, @$CombinedJavascript);
 		
 		if (count($CombinedCss) > 0) {
 			foreach ($CombinedCss as $Group => $Files) {
@@ -168,9 +185,12 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 					file_put_contents($CachedFilePath, $Combined);
 				}
 				$CssTag[HeadModule::SORT_KEY] += 1;
-				$Tags[] = array_merge($CssTag, array('href' => Asset($CachedFilePath)));
+				// This is not works...
+				// $CssTag['href'] = Asset($CachedFilePath);
+				// $Tags[] = $CssTag;
+				// This works...
+				$Tags[] = array_merge($CssTag, array('href' => Asset($CachedFilePath, False, False)));
 			}
-			
 		}
 		// TODO: IF ONE FILE IN GROUP NO NEED TO PARSE/COMBINE IT
 		if (count($CombinedJavascript) > 1) {
@@ -186,22 +206,71 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 					file_put_contents($CachedFilePath, $Combined);
 				}
 				
-				$JsTag['src'] = Asset($CachedFilePath);
+				$JsTag['src'] = Asset($CachedFilePath, False, False);
 				$JsTag[HeadModule::SORT_KEY] += 1;
 				$Tags[] = $JsTag;
 			}
 		}
 		
 		if (count($RemoveIndex) > 0) $Tags = self::RemoveKeyFromArray($Tags, Flatten($RemoveIndex));
-
+		//d($Tags);
 		$Head->Tags($Tags);
 	}
 	
-	protected static function GetGroupName($Path) {
+	public function Tick_Match_00_Minutes_05_Hours_1_Day_Handler() {
+		$Directory = new RecursiveDirectoryIterator('cache/ps');
+		foreach (new RecursiveIteratorIterator($Directory) as $File) {
+			$CachedFile = $File->GetRealPath();
+			unlink($CachedFile);
+			Console::Message('Removed ^3%s', $CachedFile);
+		}
+	}
+	
+	public function Setup() {
+		if (!is_dir('cache/ps')) mkdir('cache/ps', 0777, True);
+		
+	}
+	
+	protected function GetCachedFilePath(&$Tag, $FileKey, &$FilePath) {
+		$Url =& $Tag[$FileKey];
+		$FilePath = Null;
+		if (self::IsUrl($Url)) return False;
+		// Since 2.0.18 we have _path key.
+		if (array_key_exists('_path', $Tag)) $FilePath = $Tag['_path'];
+		else $FilePath = PATH_ROOT . parse_url($Url, PHP_URL_PATH);
+		$Basename = pathinfo($FilePath, PATHINFO_BASENAME);
+		// TODO: custom-
+		// if (substr($Basename, 0, 6) == 'custom') d($Tags, $Basename);	
+		switch ($Basename) {
+			case 'jquery.js': {
+				$Version = GetValueR('CDN.jquery', $this->Configuration, '1.4.1');
+				$Url = 'http://ajax.googleapis.com/ajax/libs/jquery/'.$Version.'/jquery.min.js';
+				return False;
+			}
+			//case 'jquery.ui.packed.js': 
+			case 'jqueryui.js': {
+				$Version = GetValueR('CDN.jqueryui', $this->Configuration, '1.7.1');
+				$Url = 'http://ajax.googleapis.com/ajax/libs/jqueryui/'.$Version.'/jquery-ui.min.js';
+				return False;
+			}
+			case 'jquery-ui.css': {
+				$Version = GetValueR('CDN.jqueryui', $Configuration, '1.7.1');
+				$Theme = GetValueR('CDN.jqueryui-theme', $Configuration, 'smoothness');
+				$Url = 'http://ajax.googleapis.com/ajax/libs/jqueryui/'.$Version.'/themes/'.$Theme.'/jquery-ui.css';
+				return False;
+			}
+			default: // Nothing
+		}
+		
+		$Hash = sprintf('%u', crc32($FilePath.filemtime($FilePath)));
+		$CachedFilePath = "cache/ps/{$Hash}.{$Basename}";
+		return $CachedFilePath;
+	}
+	
+	protected static function GetGroupName($FilePath) {
 		static $WebRootLength;
 		if (is_null($WebRootLength)) $WebRootLength = strlen(Gdn_Url::WebRoot());
-		// TODO: FIX FOR DIRECTORY
-		$GroupName = GetValue(1, explode('/', substr($Path, $WebRootLength)));
+		$GroupName = GetValue(1, explode('/', substr($FilePath, $WebRootLength)));
 		return $GroupName;
 	}
 	
@@ -221,6 +290,7 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 		$css = str_replace( '} ', '}', $css );
 		$css = str_replace( ';}', "}\n", $css );
 		$css = trim($css);
+		// TODO: REMOVE EMPTY RULES
 		return $css;
 	}
 	
@@ -239,7 +309,7 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 				$ImportMatch = $Match[0][$N];
 				$Replace[$ImportMatch] = "\n".file_get_contents($ImportFilepath);
 			} else {
-				if (defined('DEBUG')) trigger_error("FIX ME ($Filename).");
+				if (C('Debug')) trigger_error("File lost ($Filename).");
 				return $CssText;
 			}
 		}
@@ -262,18 +332,99 @@ class PageSpeedPlugin implements Gdn_IPlugin {
 		return self::StaticMinify($Text);
 	}
 	
-	public function Tick_Match_00_Minutes_05_Hours_1_Day_Handler() {
-		$Directory = new RecursiveDirectoryIterator('cache/ps');
-		foreach (new RecursiveIteratorIterator($Directory) as $File) {
-			$CachedFile = $File->GetRealPath();
-			unlink($CachedFile);
-			Console::Message('Removed ^3%s', $CachedFile);
-		}
+	protected static function RemoveKeyFromArray($Array, $Keys) {
+		// RemoveKeyFromArray in functions.general.php doesnt work as expected
+		if (!is_array($Keys)) $Keys = array($Keys);
+		if (is_array($Array)) foreach ($Keys as $Key) unset($Array[$Key]);
+		return $Array;
 	}
 	
-	public function Setup() {
-		if (!is_dir('cache/ps')) mkdir('cache/ps', 0777, True);
+	protected static function HashSumFiles($Files) {
+		$Files = array_unique($Files);
+		$HashSum = array_sum(array_map('crc32', $Files));
+		$NewHash = sprintf('%u', crc32($HashSum));
+		return $NewHash;
+	}
+	
+	/*
+	* Loads/saves image dimension
+	*/
+	protected static function ImageDimensions($NewImageDimensions = Null) {
+		$CacheFile = PATH_CACHE . '/image_dimensions.ini';
+		if ($NewImageDimensions !== Null) {
+			$PhpCode = "<?php\n\$_ = " . var_export($NewImageDimensions, True) . ';';
+			return file_put_contents($CacheFile, $PhpCode);
+		}
+		$_ = array();
+		if (file_exists($CacheFile)) include $CacheFile;
+		return $_;
+	}
+	
+	protected static function StaticPhpQueryReplace(&$String) {
 		
+		$ImageDimensions = self::ImageDimensions();
+		$Domain = Gdn::Request()->Domain();
+		
+		$Doc = PqDocument($String, array('FixHtml' => False));
+		foreach (Pq('img[src]') as $ImgNode) {
+			$PqImg = Pq($ImgNode);
+			if (!$PqImg->Attr('width') && !$PqImg->Attr('height')) {
+				// TODO: DONT USE AbsoluteSource() for relative paths
+				$Src = AbsoluteSource(trim($PqImg->Attr('src'), '/'), $Domain);
+				$CrcKey = crc32($Src);
+				if (!array_key_exists($CrcKey, $ImageDimensions)) {
+					$ImageSize = getimagesize($Src);
+					$ImageDimensions[$CrcKey] = array($ImageSize[0], $ImageSize[1], '_src' => $Src);
+				} else {
+					$ImageSize = $ImageDimensions[$CrcKey];
+				}
+				$PqImg->Attr('width', $ImageSize[0]);
+				$PqImg->Attr('height', $ImageSize[1]);
+			}
+		}
+		// Save cache.
+		self::ImageDimensions($ImageDimensions);
+		$String = $Doc->document->saveXML();
+	}
+	
+	protected static function StaticDomDocumentReplace(&$String) {
+		
+		if (substr(ltrim($String), 0, 5) != '<?xml') $String = '<?xml version="1.0" encoding="UTF-8"?'. ">\n" . $String;
+		$DOMDocument = DOMDocument::LoadXML($String, LIBXML_NOERROR);
+		if ($DOMDocument === False) return $String;
+		
+		$ImageDimensions = self::ImageDimensions();
+		$BeforeCount = count($ImageDimensions);
+		$Domain = Gdn::Request()->Domain();
+
+		$DomNodeList = $DOMDocument->GetElementsByTagName('img');
+		for ($i = $DomNodeList->length - 1; $i >= 0; $i--) {
+			$Node = $DomNodeList->Item($i);
+			$Width = $Node->GetAttribute('width');
+			$Height = $Node->GetAttribute('height');
+			if ($Width == '' && $Height == '') {
+				$Src = $Node->GetAttribute('src');
+				if (!$Src) continue;
+				$Src = AbsoluteSource($Src, $Domain);
+				$CrcKey = crc32($Src);
+				if (!array_key_exists($CrcKey, $ImageDimensions)) {
+					$ImageSize = getimagesize($Src);
+					$ImageDimensions[$CrcKey] = array($ImageSize[0], $ImageSize[1], '_src' => $Src);
+				} else {
+					$ImageSize = $ImageDimensions[$CrcKey];
+				}
+				if ($ImageSize[0] && $ImageSize[1]) {
+					$Node->SetAttribute('width', $ImageSize[0]);
+					$Node->SetAttribute('height', $ImageSize[1]);
+				}
+			}
+		}
+		// Save cache.
+		if (count($ImageDimensions) != $BeforeCount) self::ImageDimensions($ImageDimensions);
+		$String = $DOMDocument->saveXML(); // LIBXML_NOXMLDECL | LIBXML_NOENT 
+		//$DOMDocument->formatOutput = True;
+		//$String = $DOMDocument->saveXML(Null, LIBXML_NOXMLDECL | LIBXML_NOENT);
+		//d($String);
 	}
 	
 }
